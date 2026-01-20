6ca03cd6d393a296b659ef8312db1caf58dd6792
Vinit Chordiya <vinitchordiya03@gmail.com>
2026-01-20 19:29:32 +0530

    made some common packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middelware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = express();
 
 app.post("/signup", (req, res)=>{
     //db call
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
 
     res.json({
         userId:123
@@ -14,6 +23,14 @@ app.post("/signup", (req, res)=>{
 })
 
 app.post("/signin", (req, res)=>{
+
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
     const userId = 1;
     const token = jwt.sign({userId}, JWT_SECRET);
     res.json({
@@ -23,6 +40,14 @@ app.post("/signin", (req, res)=>{
 
 app.post("/room", middleware, (req, res)=>{
     //db call
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
 
     res.json({
         roomId : 123
