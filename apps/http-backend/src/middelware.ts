c22f4f89aa6d805aad39e5199c0aaa69c425290a
Vinit Chordiya <vinitchordiya03@gmail.com>
2026-01-20 18:40:06 +0530

    exposed endpoints for signing and room. also a middleware

---
+++ apps/http-backend/src/middelware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+interface AuthJwtPayload extends JwtPayload {
+  userId: string;
+}
+
+export function middleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (
+      typeof decoded === "object" &&
+      "userId" in decoded
+    ) {
+      req.userId = (decoded as AuthJwtPayload).userId;
+      return next();
+    }
+
+    return res.status(403).json({ message: "Unauthorized" });
+  } catch {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+}
